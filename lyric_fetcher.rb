5b77dd99f1dfcd0ae40d522388de74a92fc97ff7
Anders Fisher <atleastimtrying@gmail.com>
2014-02-15 22:24:35 +0000

    is_feminine added other filters removed regexes improved small style tweaks and new copy

--- lyric_fetcher.rb
+++ lyric_fetcher.rb
@@ -1,5 +1,6 @@
 require 'json'
 require 'rapgenius'
+require 'pry'
 
 class LyricFetcher
   def get_rating name
@@ -10,9 +11,7 @@ class LyricFetcher
         rapper: @songs.first.artist.name,
         rapper_url: @songs.first.artist.url,
         word_count: @lyrics.length,
-        bitch_count: bitch_count(@lyrics),
-        bastard_count: bastard_count(@lyrics),
-        swear_count: swear_count(@lyrics)
+        bitch_count: bitch_count(@lyrics)
       }.to_json
     else
       'no rapper found'
@@ -24,22 +23,24 @@ private
   end
 
   def bitch_count string
-    string.scan(/bitch/).length
-  end
-
-  def bastard_count string
-    string.scan(/bastard/).length
-  end
-
-  def swear_count string
-    string.scan(/shit|fuck|cunt|arse|ass|wank|bastard|bitch|twat|bugger|bollocks/).length
+    string.scan(/\ bitch\ |\ ho\ |\ hoe\ |\ ho\'s\ |\ hoes\ /).length
   end
 
   def fetch_lyrics songs
-    songs.inject("") {|lyrics, song| lyrics + get_lyrics(song) }
+    songs.inject("") { |lyrics, song| lyrics + get_lyrics(song) }
+  end
+
+  def is_feminine string
+    string.scan(/\ she\ |\ her\ |\ woman\ |\ girl\ |\ bitch\ |\ ho\ |\ lady\ |\ momma\ |\ ma\ |\ mom\ |\ hoe\ |\ ho\'s\ |\ hoes\ /).length > 0
   end
 
   def get_lyrics song
-    song.lines.map(&:lyric).join
+    lyrics = ""
+    song.lines.each do |line|
+      if is_feminine line.lyric
+        lyrics << line.lyric + " "
+      end
+    end
+    lyrics
   end
 end
