6ef497dacafb694735fdfeaa86f5b512089d806c
Anders Fisher <atleastimtrying@gmail.com>
2014-02-18 09:21:29 +0000

    favicon added closes #2

--- app.rb
+++ app.rb
@@ -15,6 +15,10 @@ get '/style.css' do
   send_file 'style.css'
 end
 
+get '/favicon.ico' do
+  send_file 'favicon.ico'
+end
+
 get '/rating.json' do
   if params[:name]
     lyric_fetcher.get_rating params[:name]
